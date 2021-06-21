cbcd4b3cb301e4644928bf178fc1828aab47df2d
Quangchien99 <phamquangchien170499@gmail.com>
2021-06-21 20:26:54 +0700

    Implement room database for NewsArticle and BreakingNewsArticle

---
+++ app/src/main/java/com/example/newsapplication/data/NewsArticle.kt
@@ -0,0 +1,22 @@
+package com.example.newsapplication.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "news_articles")
+data class NewsArticle(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val url: String,
+    val title: String?,
+    val thumbnailUrl: String,
+    val isBookmarked: Boolean,
+    val updateAt: Long = System.currentTimeMillis()
+)
+
+@Entity(tableName = "breaking_news")
+data class BreakingNews(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val articleUrl: String
+)
---
+++ app/src/main/java/com/example/newsapplication/data/NewsArticleDatabase.kt
@@ -0,0 +1,11 @@
+package com.example.newsapplication.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [NewsArticle::class, BreakingNews::class], version = 1)
+abstract class NewsArticleDatabase : RoomDatabase() {
+
+    abstract fun getNewsArticleDao(): NewsArticleDao
+
+}
